8a24324a198e211eb4e94ca820640a1551e2c46d
stdg3 <velikoglosv@mail.ru>
2022-10-12 16:33:51 +0300

    branchCheck

---
+++ src/CCPDemo.Web.Mvc/Areas/App/Controllers/aaaaaatest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CCPDemo.Web.Areas.App.Controllers
+{
+    public class aaaaaatest : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
