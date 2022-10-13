d7422238bf04143ebd39f5d5cbf1393635dfc2e6
stdg3 <velikoglosv@mail.ru>
2022-10-13 11:46:22 +0300

    isDoneModule2CreatingThePage

--- src/CCPDemo.Web.Mvc/Areas/App/Controllers/PhoneBookController.cs
+++ src/CCPDemo.Web.Mvc/Areas/App/Controllers/PhoneBookController.cs
@@ -1,4 +1,8 @@
-﻿using CCPDemo.Web.Controllers;
+﻿using Abp.AutoMapper;
+using CCPDemo.Dto;
+using CCPDemo.InterfacePerson;
+using CCPDemo.Web.Areas.App.Models.PhoneBook;
+using CCPDemo.Web.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CCPDemo.Web.Areas.App.Controllers
@@ -6,9 +10,17 @@ namespace CCPDemo.Web.Areas.App.Controllers
     [Area("App")]
     public class PhoneBookController : CCPDemoControllerBase
     {
-        public ActionResult Index()
+        private readonly IPersonAppService _personAppService;
+
+        public PhoneBookController(IPersonAppService personAppService)
         {
-            return View();
+            _personAppService = personAppService;
+        }
+        public ActionResult Index(GetPeopleInput input)
+        {
+            var output = _personAppService.GetPeople(input);
+            var model = ObjectMapper.Map<IndexViewModel>(output);
+            return View(model);
         }
     }
 }
---
+++ src/CCPDemo.Web.Mvc/Areas/App/Models/PhoneBook/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using CCPDemo.Dto;
+
+namespace CCPDemo.Web.Areas.App.Models.PhoneBook
+{
+    [AutoMapFrom(typeof(ListResultDto<PersonListDto>))]
+    public class IndexViewModel : ListResultDto<PersonListDto>
+    {
+
+    }
+}
