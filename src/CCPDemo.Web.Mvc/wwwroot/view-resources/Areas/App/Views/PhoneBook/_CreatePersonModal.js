7e75310087ef0c398a72ad3d386fabbc1adac7fb
stdg3 <velikoglosv@mail.ru>
2022-10-13 14:53:11 +0300

    isDoneSavingPersonEndOfModule2

--- src/CCPDemo.Web.Mvc/wwwroot/view-resources/Areas/App/Views/PhoneBook/_CreatePersonModal.js
+++ src/CCPDemo.Web.Mvc/wwwroot/view-resources/Areas/App/Views/PhoneBook/_CreatePersonModal.js
@@ -1,15 +1,30 @@
 ﻿(function ($) {
     app.modals.CreatePersonModal = function () {
         var _modalManager;
+        var _personService = abp.services.app.person;
+        var _$form = null;
 
         this.init = function (modalManager) {
             _modalManager = modalManager;
 
-            //Initialize your modal here...
+            _$form = _modalManager.getModal().find('form');
+            _$form.validate();
         };
 
         this.save = function () {
-            //Save your modal here...
+            if (!_$form.valid()) {
+                return;
+            }
+
+            var person = _$form.serializeFormToObject();
+
+            _modalManager.setBusy(true);
+            _personService.createPerson(person).done(function () {
+                _modalManager.close();
+                location.reload();
+            }).always(function () {
+                _modalManager.setBusy(false);
+            });
         };
     };
 })(jQuery);
