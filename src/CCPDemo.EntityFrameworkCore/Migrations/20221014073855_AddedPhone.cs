1dcbfbe19d8845062dc1ea8cd457137800a472df
stdg3 <velikoglosv@mail.ru>
2022-10-14 10:46:25 +0300

    isDoneDataBaseMigrationOfPhoneEntity

---
+++ src/CCPDemo.EntityFrameworkCore/Migrations/20221014073855_AddedPhone.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CCPDemo.Migrations
+{
+    public partial class AddedPhone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PbPhones",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<byte>(type: "tinyint", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PbPhones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PbPhones_PbPersons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "PbPersons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PbPhones_PersonId",
+                table: "PbPhones",
+                column: "PersonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PbPhones");
+        }
+    }
+}
---
+++ src/CCPDemo.EntityFrameworkCore/Migrations/Seed/Host/InitialPeopleAndPhoneCreator.cs
@@ -0,0 +1,59 @@
+﻿using CCPDemo.EntityFrameworkCore;
+using CCPDemo.Persons;
+using CCPDemo.Phones;
+using CCPDemo.PhoneTypes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCPDemo.Migrations.Seed.Host
+{
+    public class InitialPeopleAndPhoneCreator
+    {
+        private readonly CCPDemoDbContext _context;
+
+        public InitialPeopleAndPhoneCreator(CCPDemoDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            var douglas = _context.Persons.FirstOrDefault(p => p.EmailAddress == "douglas.adams@fortytwo.com");
+            if (douglas == null)
+            {
+                _context.Persons.Add(
+                    new Person
+                    {
+                        Name = "Douglas",
+                        Surname = "Adams",
+                        EmailAddress = "douglas.adams@fortytwo.com",
+                        Phones = new List<Phone>
+                                    {
+                                    new Phone {Type = PhoneType.Home, Number = "1112242"},
+                                    new Phone {Type = PhoneType.Mobile, Number = "2223342"}
+                        }
+                    });
+            }
+
+            var asimov = _context.Persons.FirstOrDefault(p => p.EmailAddress == "isaac.asimov@foundation.org");
+            if (asimov == null)
+            {
+                _context.Persons.Add(
+                    new Person
+                    {
+                        Name = "Isaac",
+                        Surname = "Asimov",
+                        EmailAddress = "isaac.asimov@foundation.org",
+                        Phones = new List<Phone>
+                                    {
+                                    new Phone {Type = PhoneType.Home, Number = "8889977"}
+                                    }
+                    });
+            }
+        }
+    }
+}
