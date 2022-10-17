6a303691ffba2b8cc2457e91921db5101a94e31e
stdg3 <velikoglosv@mail.ru>
2022-10-17 17:03:10 +0300

    isDoneGetPhoneTypeMethodFromMVCController

---
+++ src/CCPDemo.Web.Mvc/Areas/App/Controllers/PhoneTypeController.cs
@@ -0,0 +1,26 @@
+﻿using CCPDemo.Dto;
+using CCPDemo.InterfacePhoneType;
+using CCPDemo.Web.Areas.App.Models.PhoneBook.PhoneType;
+using CCPDemo.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCPDemo.Web.Areas.App.Controllers
+{
+    [Area("App")]
+    public class PhoneTypeController : CCPDemoControllerBase
+    {
+        private readonly IPhoneTypeService _phoneTypeService;
+
+        public PhoneTypeController(IPhoneTypeService phoneTypeService)
+        {
+            _phoneTypeService = phoneTypeService;
+        }
+
+        public IActionResult Index(GetPhoneTypeInput input)
+        {
+            var output = _phoneTypeService.GetPhoneType(input);
+            var model = ObjectMapper.Map<IndexViewModel>(output);
+            return View(model);
+        }
+    }
+}
---
+++ src/CCPDemo.Web.Mvc/Areas/App/Models/PhoneBook/PhoneType/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using CCPDemo.Dto;
+
+namespace CCPDemo.Web.Areas.App.Models.PhoneBook.PhoneType
+{
+    [AutoMapFrom(typeof(ListResultDto<PhoneTypeListDto>))]
+    public class IndexViewModel : ListResultDto<PhoneTypeListDto>
+    {
+
+    }
+}
